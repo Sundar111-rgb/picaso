6374ffbf99c0bd878c4ec28befe7e9194deb0da3
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2022-02-03 22:55:39 +0530

    Update ArrayResearch.js

--- ArrayResearch.js
+++ ArrayResearch.js
@@ -10,3 +10,10 @@ const ggg = [6,7,8,9,4];
 console.log((hhh.length-1) - ggg.pop())
 
 output :- 1
+
+const hhh = [1,2,3,4,5];
+const ggg = [6,7,8,9,10];
+console.log([...hhh,...ggg].sort((a,b) => b-a))
+
+output :- [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
