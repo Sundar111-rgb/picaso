c3bdaf09127e996eb7947a3511bb72da50e1e0d3
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2022-02-03 22:51:52 +0530

    Update ArrayResearch.js

--- ArrayResearch.js
+++ ArrayResearch.js
@@ -3,3 +3,10 @@ const ggg = [6,7,8,9,10];
 console.log([...hhh,...ggg].map(item => item*2))
 
 output :-  [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+
+const hhh = [1,2,3,4,5,6];
+const ggg = [6,7,8,9,4];
+console.log((hhh.length-1) - ggg.pop())
+
+output :- 1
