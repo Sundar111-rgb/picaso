703ab8c5995788603141b3a5e4dd3bb54afd3eb8
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2022-06-20 23:57:05 +0530

    Create JqeryAjax.js

---
+++ src/JqeryAjax.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+  
+<script type="text/javascript">
+    
+   $(document).ready(() => {
+   $.ajax({
+    type: "get",
+    url: "https://jsonplaceholder.typicode.com/todos/"+('#id').val(),
+    dataType: "json",
+    success: (res) => {
+      var html = '';
+            $.each(res, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.id + '</td>';
+                html += '<td>' + item.userId + '</td>';
+                html += '<td>' + item.title + '</td>';
+                html += '<td>' + item.completed + '</td>';
+                html += '<td><a href="#" onclick="return getbyID(' + item.id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.id + ')">Delete</a></td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+    }
+     
+   });
+     
+     
+});
+    
+</script>
+  <table>
+    <thead>
+    <tr>
+       <td><input type="text" id="id" /></td>
+       <td><input type="text" id="name" /></td>
+       <td><input type="text" id="address" /></td>
+    </tr>
+    </thead>
+   <thead>
+        <tr>
+             <th>ID</th>
+                    <th>
+                        UserID
+                    </th>
+                    <th>
+                        Title
+                    </th>
+                   
+                    <th>
+                        Completed
+                    </th>
+                    <th>
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="tbody">
+
+            </tbody>
+  </table>
+
+</body>
+</html>
