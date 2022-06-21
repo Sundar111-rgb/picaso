ca67277e412a4690edda172285b0fbbb951f37b6
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2022-06-21 17:09:20 +0530

    Update JqeryAjax.js

--- src/JqeryAjax.js
+++ src/JqeryAjax.js
@@ -11,25 +11,51 @@
 <script type="text/javascript">
     
    $(document).ready(() => {
-   $.ajax({
-    type: "get",
-    url: "https://jsonplaceholder.typicode.com/todos/"+('#id').val(),
+     
+     const getbyID = (id) => {
+       console.log(id)
+     }
+    
+     $("#btn").click(() => { 
+     $.ajax({
+        type: "get",
+         url: "https://jsonplaceholder.typicode.com/todos/",
     dataType: "json",
-    success: (res) => {
+     success: (res) => {
       var html = '';
-            $.each(res, function (key, item) {
+            $.each(res,(key, item) => {
                 html += '<tr>';
                 html += '<td>' + item.id + '</td>';
                 html += '<td>' + item.userId + '</td>';
                 html += '<td>' + item.title + '</td>';
                 html += '<td>' + item.completed + '</td>';
-                html += '<td><a href="#" onclick="return getbyID(' + item.id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.id + ')">Delete</a></td>';
+                html += '<td><a href="#" onclick="getbyID(' + item.id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.id + ')">Delete</a></td>';
                 html += '</tr>';
             });
             $('.tbody').html(html);
     }
+    })
+    })
      
-   });
+      $("#Get").click(() => { 
+      $.ajax({
+        type: "get",
+         url: "https://reqres.in/api/users?page="+$('#id').val(),
+    dataType: "json",
+     success: (res) => {
+      var html = '';
+            $.each(res.data,(key, item) => {
+                html += '<tr>';
+                html += '<td>' + item.id + '</td>';
+                html += '<td>' + item.first_name + '</td>';
+                html += '<td>' + item.last_name + '</td>';
+                html += '<td>' + item.email + '</td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+    }
+    })
+    })
      
      
 });
@@ -41,8 +67,11 @@
        <td><input type="text" id="id" /></td>
        <td><input type="text" id="name" /></td>
        <td><input type="text" id="address" /></td>
+       <td><button id="btn">GetData</button></td>
+       <td><button id="Get">GetDataa</button></td>
     </tr>
     </thead>
+    
    <thead>
         <tr>
              <th>ID</th>
