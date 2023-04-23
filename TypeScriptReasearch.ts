3d7517eb0546ff9e8bf105c039b918cda5d45453
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2023-04-23 15:17:16 +0530

    Create TypeScriptReasearch.ts

---
+++ TypeScriptReasearch.ts
@@ -0,0 +1,11 @@
+interface name {
+    first:string,
+    last:string
+}
+
+interface data {
+    id:number
+    attributes : name[];
+}
+
+const hhh:data = {id:1,attributes:[{ first:"Sundar", last:"Kumar"}]};
