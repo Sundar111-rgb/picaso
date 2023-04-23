c814e363d9b49c314e74d6508923b4b86320a1a5
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2023-04-23 15:30:52 +0530

    Update TypeScriptReasearch.ts

--- TypeScriptReasearch.ts
+++ TypeScriptReasearch.ts
@@ -9,3 +9,68 @@ interface data {
 }
 
 const hhh:data = {id:1,attributes:[{ first:"Sundar", last:"Kumar"}]};
+------------------------------------------------------------------------------------------------
+
+interface name {
+    first:string,
+    last:string
+}
+
+interface data {
+    id:number
+    attributes : name;
+}
+
+const hhh:data = {id:1,attributes:{ first:"Sundar", last:"Kumar"}};
+--------------------------------------------------------------------------------------------------
+
+interface name {
+    first:string,
+    last:string
+}
+
+interface email {
+    personal:string,
+    official:string
+}
+
+interface data {
+    id:number
+    attributes : { name:name[], email:email[] };
+}
+
+const hhh:data = { id:1,
+                   attributes:{ 
+                               name :[{first:"Sundar", last:"Kumar"}],
+                               email:[{personal:"kumarsun53",official:"sundar.kumar"}] 
+                      
+                   }
+    
+                 };
+-------------------------------------------------------------------------------------------------
+    
+interface  name {
+    first:string,
+    last:string
+}
+
+interface email {
+    personal:string,
+    official:string
+}
+
+interface data {
+    id:number
+    attributes : { name:name, email:email};
+}
+
+const hhh:data = { id:1,
+                   attributes:{ 
+                               name :{first:"Sundar", last:"Kumar"},
+                               email:{personal:"kumarsun53",official:"sundar.kumar"}
+                      
+                   }
+    
+                 };
+
+
