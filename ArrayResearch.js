a355deb0abd4c86c580081c23aea01c21a61dcf1
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2023-11-20 20:25:03 +0530

    Update ArrayResearch.js

--- ArrayResearch.js
+++ ArrayResearch.js
@@ -485,3 +485,49 @@ p.then(res => res.map(i => console.log(i.id+' '+i.first+' '+i.last))
 )
 
 
+------------------------------------------------------------------------------------------------
+
+	let arr = [1,1,2,3,2,3,4,4]; 
+
+function removeDuplicates(arr) { 
+	return arr.filter((item, 
+		index) => arr.indexOf(item) === index); 
+} 
+console.log(removeDuplicates(arr));
+
+function removeDuplicatess(arr) { 
+	let unique = []; 
+	arr.forEach(element => { 
+		if (!unique.includes(element)) { 
+			unique.push(element); 
+		} 
+	}); 
+	return unique; 
+} 
+console.log(removeDuplicatess(arr));
+
+function removeDuplicatesss(arr) { 
+    let unique = arr.reduce(function (acc, curr) { 
+        if (!acc.includes(curr)) 
+            acc.push(curr); 
+        return acc; 
+    }, []); 
+    return unique; 
+} 
+console.log(removeDuplicatesss(arr));
+
+function removeDuplicatessss(arr) { 
+    let unique = []; 
+    for (i = 0; i < arr.length; i++) { 
+        if (unique.indexOf(arr[i]) === -1) { 
+            unique.push(arr[i]); 
+        } 
+    } 
+    return unique; 
+} 
+console.log(removeDuplicatessss(arr));
+
+
+
+
+
