39f6fad0484d9f26018271962cffd9c271444f53
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2023-12-31 09:21:34 +0530

    Update ArrayResearch.js

--- ArrayResearch.js
+++ ArrayResearch.js
@@ -893,6 +893,34 @@ outout :-
 
 "geksford"
 
+---------------------------------------------------------------
+
+const number = 10
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+
+output :-
+
+"Fibonacci Series:"
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+
 
 	
 
