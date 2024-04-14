89b7b39a6dc6e66b757f23c265b64640368083fe
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2024-04-14 18:36:42 +0530

    Update Logics.js

--- src/Logics.js
+++ src/Logics.js
@@ -18,3 +18,50 @@ const flattenArray =(arr) => {
 
 console.log(flattenArray(hhh));
 
+/* Count substring in string */
+
+const str = "Mynameiskhan Mynameiskhan Mynameiskhan Mynameiskhan Mynameisjaan";
+var count=0;
+var khan = [];
+const countkhan = (str) => {
+  for(var i=0;i<=str.length;i++)
+    
+      for(var j=0;j<=str.length;j++)
+    
+      if(str.substr(i,j) == "My" )
+        {
+          count++;
+          khan.push(str.substr(i,j))
+        }
+    
+    
+ return count;
+}
+
+console.log(countkhan(str));
+
+/* Max and SecondMax */
+
+const hhh = [1,2,3,4,5,6];
+var max = 0;
+var secmax = 0;
+const secondMax = (hhh) => {
+  for(var i=0;i<=hhh.length-1;i++)
+    {
+      if(max < hhh[i])
+        {
+          secmax = max;
+          max = hhh[i]
+        }
+      else if(hhh[i] != max && secmax<hhh[i])
+        {
+          secmax = hhh[i]
+        }
+   }
+}
+
+secondMax(hhh)
+
+console.log(max, secmax);
+
+
